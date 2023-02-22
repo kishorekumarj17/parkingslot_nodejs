7cfcb7f46ec42e28402a8ab1cbe4df92d3287fb3
kishorekumarj17 <42991864+kishorekumarj17@users.noreply.github.com>
2023-02-22 12:36:26 +0530

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -4,6 +4,6 @@ var serviceAccount = require("../config/serviceAccountKey.json");
 
 const db=firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
-    databaseURL: "http://parkingslot-a9644.firebaseio.com"
+    databaseURL: "http://###dbid###.firebaseio.com"
 });
 module.exports = db;
